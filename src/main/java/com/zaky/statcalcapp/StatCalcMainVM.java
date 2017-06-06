2db27e91370fbdfe5a35235a8bbbcd5e4205202d
zakymukhoyyar <zakymukhoyyar@gmail.com>
2017-06-06 10:15:45 +0700

    Initial Commit

---
+++ src/main/java/com/zaky/statcalcapp/StatCalcMainVM.java
@@ -0,0 +1,49 @@
+package com.zaky.statcalcapp;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+
+/**
+ *
+ * @author Zaky
+ */
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
+public class StatCalcMainVM {
+    
+    private String name;
+    private String clas;
+    private int baseLv;
+    private int jobLv;
+    
+    private int baseStr;
+    private int bonusStr;
+    private int totalStr;
+    
+    private int baseAgi;
+    private int bonusAgi;
+    private int totalAgi;
+    
+    private int baseVit;
+    private int bonusVit;
+    private int totalVit;
+    
+    private int baseInt;
+    private int bonusInt;
+    private int totalInt;
+        
+    private int baseDex;
+    private int bonusDex;
+    private int totalDex;
+    
+    private int baseLuk;
+    private int bonusLuk;
+    private int totalLuk;
+    
+    
+    private List<Integer> pRenew = new ArrayList<Integer>();
+    
+    
+    
+    
+}
